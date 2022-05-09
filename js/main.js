8f45855be48a793b5b7f74704bb657ee949fa8ed
Suraj Patil <suraj.gd89@gmail.com>
2022-05-09 19:18:23 +0530

    app-name added

--- js/main.js
+++ js/main.js
@@ -28,6 +28,8 @@ equal.addEventListener("click", function () {
     calculation.textContent = result.textContent;
 });
 
+
+// THEME SWITCH
 light.addEventListener("click", function () {
     document.body.classList.remove('dark');
     document.body.classList.add('light');
@@ -53,7 +55,6 @@ if (document.body.classList.contains('light')) {
 
 
 //TIME
-
 var nowdate = new Date();
 function currentTime(nowdate) {
     var hours = nowdate.getHours();
@@ -66,6 +67,7 @@ function currentTime(nowdate) {
     return strTime;
 }
 
+//DATE
 function currentDate(nowdate) {
     var result = nowdate.toLocaleDateString("en-GB", {
         year: "numeric",
@@ -83,4 +85,9 @@ setInterval(function () {
     date.textContent = currentdate;
 }, 1000)
 
-
+//LINK Animation
+document.querySelectorAll('.link').forEach(link => {
+    link.innerHTML = '<div><span>' + link.textContent.trim().split('').join('</span><span>') + '</span></div>'
+    link.querySelectorAll('span').forEach(s => s.innerHTML = s.textContent == ' ' ? '&nbsp;' : s.textContent)
+    link.insertAdjacentHTML('beforeend', '<div><svg preserveAspectRatio="none" viewBox="0 0 192 5"><path d="M191.246 4H129C129 4 127.781 4.00674 127 4C114.767 3.89447 108.233 1 96 1C83.7669 1 77.2327 3.89447 65 4C64.219 4.00674 63 4 63 4H0.751923" /></svg></div>')
+});
